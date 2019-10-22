cc5c54fcdc6ce3b943668d4fdf2dcf42ddf22cc0
vincenttang <10290348+vincentntang@users.noreply.github.com>
2019-10-22 08:00:26 -0400

    simplified coding example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,37 @@
 import React, {useState, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 import movieData from './movies.json'
+import quotesData from './quotes.json'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h2>Movie Searcher</h2>
       <input className="movie-search"/>
-      <section className="movie-list">
-        {movieData.map(item => {
+      <div className="movie-list">
+        {movieData.map(movie => {
           return (
             <div className="movie">
-              <div className="movie-title">{item.Title}</div>
-              <div className="movie-year">{item.Year}</div>
-              <div className="movie-genre">{item.Genre}</div>
-              <img className="movie-img" src={item.Poster}/>
+              <img src={movie.Poster}/>
+              <section>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>                
+                <ul>
+                  {quotesData.map(quote => {
+                    if(quote.imdbID === movie.imdbID){
+                      return (
+                        <li key={quote.imdbID}>
+                          {quote.description}
+                        </li>
+                      )
+                    }
+                  })}
+                </ul>
+              </section>
             </div>
           )
         })}
-      </section>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Movie.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default Movie = (props) =>{
+  return (
+    <div>
+      
+    </div>
+  )
+}
