b5ad9e90b709184ee59867913c6d1c720b40d15e
vincenttang <10290348+vincentntang@users.noreply.github.com>
2019-10-24 09:32:54 -0400

    add list of todos

--- frontend/src/components/MovieCreate.js
+++ frontend/src/components/MovieCreate.js
@@ -6,6 +6,17 @@ export const MovieCreate = (props) =>{
       <form>
         <input type="text"/>
         <input type="text"/>
+        <input type="text"/>
+        {/* TODO - Format the code here */}
+        {/* TODO - When you click submit, show an alert that does nothing. 
+        Push back to the original location route as well. In the backend
+        console log the result and show what's supposed to return
+        show network tab as well 
+        fork branch, create backend
+        branch master - finished project w/ sequelize deploy to heroku
+        branch 01 - starter - starting part
+        branch 02 - fullstack but no database
+        branch 03 - deployed, sequelize (same as master) */}
       </form>
     </div>
   )
